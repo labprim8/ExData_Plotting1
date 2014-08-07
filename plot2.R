8de76e1e8ebb97a82468c445d68705ae19424140
gary nugent <gary.nugent@gmail.com>
2014-08-07 10:14:09 +0100

    fixed error in data manipulation

--- plot2.R
+++ plot2.R
@@ -1,7 +1,7 @@
 ## read in file
 power <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?", stringsAsFactors=F)
 ## create a new column to hold the correct date time
-power$dateTime <- strptime(paste(power$dateTime), format="%d/%m/%Y %H:%M:%S")
+power$dateTime <- strptime(paste(power$Date, power$Time), format="%d/%m/%Y %H:%M:%S")
 ##subset the data to show only 01/02/2007 and 02/02/2007
 subset <- subset(power, power$dateTime < "2007-02-03" & power$dateTime >= "2007-02-01" )
 
